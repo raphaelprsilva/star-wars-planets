d58b7916b5ba64eb3a51e676a2aeb71cebc50ede
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-11-02 10:20:08 -0300

    feat: add PlanetsFilters component to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-import Table from './components/Table';
 import PlanetsProvider from './context/PlanetsProvider';
+
+import PlanetsFilters from './components/PlanetsFilters';
+import Table from './components/Table';
 
 function App() {
   return (
     <PlanetsProvider>
       <>
         <h1>Star Wars Planets</h1>
+        <PlanetsFilters />
         <Table />
       </>
     </PlanetsProvider>
