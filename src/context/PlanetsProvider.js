8ae51b12fe31f3631906ab888477b08945837ff8
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-11-02 10:17:56 -0300

    feat: provides filters to App

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -3,13 +3,17 @@ import React from 'react';
 
 import usePlanets from '../hooks/usePlanets';
 import PlanetsContext from './PlanetsContext';
+import usePlanetsFilters from '../hooks/usePlanetsFilters';
 
 const PlanetsProvider = ({ children }) => {
   const { planets, loading } = usePlanets(null);
+  const [filters, setFilters] = usePlanetsFilters();
 
   const contextValue = {
     data: planets,
     loading,
+    filters,
+    setFilters,
   };
 
   return (
