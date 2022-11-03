68c895335f796186d3affa720da5e3ab9135ab84
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-11-03 17:16:41 -0300

    feat: implements logic to filter by numeric values

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -4,69 +4,83 @@ import PlanetsContext from '../../context/PlanetsContext';
 
 const Table = () => {
   const { data: planets, loading, filters } = useContext(PlanetsContext);
-  const { filterByName: { name }, filterByNumericValues } = filters;
-  const { column, comparison, value } = filterByNumericValues;
+  const {
+    filterByName: { name },
+    filterByNumericValues,
+  } = filters;
+
+  const filterPlanetsByNumericValues = (arr) => {
+    if (arr.length > 0) {
+      return arr.reduce((acc, { column, comparison, value }) => {
+        switch (comparison) {
+        case 'maior que':
+          return acc.filter(
+            (planet) => Number(planet[column]) > Number(value),
+          );
+        case 'menor que':
+          return acc.filter(
+            (planet) => Number(planet[column]) < Number(value),
+          );
+        case 'igual a':
+          return acc.filter(
+            (planet) => Number(planet[column]) === Number(value),
+          );
+        default:
+          return acc;
+        }
+      }, planets);
+    }
+    return planets;
+  };
+
+  const filteredByNumericValues = filterPlanetsByNumericValues(filterByNumericValues);
+  const planetsToRender = filteredByNumericValues || planets;
 
   const setTableColumns = (planetsData) => {
     const columnsKeys = Object.keys(planetsData[0]);
-    const columnsWithoutResident = columnsKeys
-      .filter((col) => col !== 'residents');
-    const columnsNames = columnsWithoutResident
-      .map((col) => col.replace('_', ' '));
+    const columnsWithoutResident = columnsKeys.filter(
+      (col) => col !== 'residents',
+    );
+    const columnsNames = columnsWithoutResident.map((col) => col.replace('_', ' '));
     const columnsNamesCapitalized = columnsNames.map(
       (col) => col.charAt(0).toUpperCase() + col.slice(1),
     );
     return columnsNamesCapitalized;
   };
 
-  return (
-    loading ? (
-      <div>Loading...</div>
-    ) : (
-      <table>
-        <thead>
-          <tr>
-            {setTableColumns(planets)
-              .map((tableColumn) => (
-                <th key={ tableColumn }>{tableColumn}</th>
-              ))}
-          </tr>
-        </thead>
-        <tbody>
-          {planets
-            .filter((planet) => planet.name.includes(name))
-            .filter((planet) => {
-              if (comparison === 'maior que') {
-                return +planet[column] > +value;
-              }
-              if (comparison === 'menor que') {
-                return +planet[column] < +value;
-              }
-              if (comparison === 'igual a') {
-                return +planet[column] === +value;
-              }
-              return true;
-            })
-            .map((planet) => (
-              <tr key={ planet.name }>
-                <td>{planet.name}</td>
-                <td>{planet.rotation_period}</td>
-                <td>{planet.orbital_period}</td>
-                <td>{planet.diameter}</td>
-                <td>{planet.climate}</td>
-                <td>{planet.gravity}</td>
-                <td>{planet.terrain}</td>
-                <td>{planet.surface_water}</td>
-                <td>{planet.population}</td>
-                <td>{planet.films}</td>
-                <td>{planet.created}</td>
-                <td>{planet.edited}</td>
-                <td>{planet.url}</td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-    )
+  return loading ? (
+    <div>Loading...</div>
+  ) : (
+    <table>
+      <thead>
+        <tr>
+          {setTableColumns(planets).map((tableColumn) => (
+            <th key={ tableColumn }>{tableColumn}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {planetsToRender
+          .filter((planet) => planet.name.includes(name))
+          .map((planet) => (
+            <tr key={ planet.name }>
+              <td>{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>{planet.films}</td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
   );
 };
 
