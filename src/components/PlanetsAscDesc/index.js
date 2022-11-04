abe19ac864a4bcf0e5f0f953b0ad49d45519f66b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-11-04 10:22:28 -0300

    style: add PlanetsAscDesc style

--- src/components/PlanetsAscDesc/index.js
+++ src/components/PlanetsAscDesc/index.js
@@ -1,9 +1,13 @@
 import React, { useState, useContext } from 'react';
+
 import Select from '../Select';
+import Input from '../Input';
+import Button from '../Button';
+
+import * as S from './styled';
 
 import PlanetsContext from '../../context/PlanetsContext';
 import { columnsOptions } from '../../helpers/options';
-import Input from '../Input';
 
 const PlanetsAscDesc = () => {
   const [column, setColumn] = useState('population');
@@ -22,7 +26,7 @@ const PlanetsAscDesc = () => {
   };
 
   return (
-    <form onSubmit={ setAscDescFilter }>
+    <S.FormWrapperAscDesc onSubmit={ setAscDescFilter }>
       <Select
         name="column-sort"
         label="Ordenar:"
@@ -30,31 +34,32 @@ const PlanetsAscDesc = () => {
         handleChange={ ({ target }) => setColumn(target.value) }
         options={ columnsOptions }
       />
-      <Input
-        dataTestId="column-sort-input-asc"
-        id="column-sort-input-asc"
-        label="Ascendente"
-        name="order"
-        type="radio"
-        value={ order }
-        handleChange={ () => setOrder('ASC') }
-      />
-      <Input
-        dataTestId="column-sort-input-desc"
-        id="column-sort-input-desc"
-        label="Descendente"
-        name="order"
-        type="radio"
-        value={ order }
-        handleChange={ () => setOrder('DESC') }
-      />
-      <button
+      <S.InputsWrapper>
+        <Input
+          dataTestId="column-sort-input-asc"
+          id="column-sort-input-asc"
+          label="Ascendente"
+          name="order"
+          type="radio"
+          value={ order }
+          handleChange={ () => setOrder('ASC') }
+        />
+        <Input
+          dataTestId="column-sort-input-desc"
+          id="column-sort-input-desc"
+          label="Descendente"
+          name="order"
+          type="radio"
+          value={ order }
+          handleChange={ () => setOrder('DESC') }
+        />
+      </S.InputsWrapper>
+      <Button
         type="submit"
         data-testid="column-sort-button"
-      >
-        Ordernar
-      </button>
-    </form>
+        name="Ordenar"
+      />
+    </S.FormWrapperAscDesc>
   );
 };
 
---
+++ src/components/PlanetsAscDesc/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { FormWrapper } from '../PlanetsFilterForm/styled';
+
+export const FormWrapperAscDesc = styled(FormWrapper)``;
+
+export const InputsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
